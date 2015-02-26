198564c4b505dd9dc05ebf85727a2f00a814aa50
xljsskk <jli@ccs.neu.edu>
2015-02-25 23:29:25 -0500

    Assignment second edition

--- Assignment4/dag-single-source.py
+++ Assignment4/dag-single-source.py
@@ -39,31 +39,38 @@ class Solution:
         self.dist.append(float("inf"))
         self.prev = []
         self.prev.append(None)
-        self.iteration = 1
         self.typo = [3, 9, 8, 5, 4, 6, 7, 2, 1, 10]
+        self.flag = True
 
     def bellman(self,G,l,s):
         for u in G.keys():
             self.dist.append(float('inf'))
             self.prev.append(None)
         self.dist[s] = 0.0
-        print "---iteration %d:---"%(self.iteration)
         #print "changset is %r"%(self.changeset)
+        print "---initial distance---"
         self.getdist()
 
-        for u in self.typo:
-            self.iteration += 1
-            print "---iteration %d:---"%(self.iteration)
-            for v in G[u]:
-                    self.update(u,v,l)
+        if self.flag:
+            self.flag = False
+            for u in self.typo:
+                print "---update edges from vertex%d:---"%(u)
+                for v in G[u]:
+                        self.update(u,v,l)
                 ###print "change set now is %r"%(self.changeset)
-            self.getdist()
+                self.getdist()
+                self.getprev()
 
     def update(self,u,v,l):
-        print " edge (%d,%d), length %.1f"%(u,v,l[(u,v)])####
+        #print " edge (%d,%d), length %.1f"%(u,v,l[(u,v)])####
         #print "dist[%d] = min(%d,%d)"%(v,self.dist[v],self.dist[u] + edgelength)###
         if self.dist[u]+ l[(u,v)]< self.dist[v]:
             self.dist[v] = self.dist[u]+ l[(u,v)]
+            self.prev[v] = u
+            self.flag = self.flag or True
+        else:
+            self.flag = self.flag or False
+
 
     def getdist(self):
         for i in range(len(self.dist) -1):
@@ -71,6 +78,14 @@ class Solution:
                 print "Node %d:INF"%(i+1)
             else:
                 print "Node %d:%.1f"%(i+1,self.dist[i+1])
+        print
+
+
+    def getprev(self):
+        for i in range(len(self.prev)-1):
+            print "(%d.prev is %r)"%(i+1,self.prev[i+1])
+        print
+
 
 a = Solution()
 a.bellman(graph,length,3)
